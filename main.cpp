6e39af509bd80c5ad61214df342546423e274802
cbiale <claudio_biale@yahoo.com.ar>
2021-05-27 11:45:15 -0300

    Cambio pin D6

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     // variable de tiempo
     Tiempo tiempo(26, 5, 2021, 22, 45, 0);
     // variable del sistema
-    Sistema sistema(tiempo, 0.3f, 22.0f, PA_0, PF_8, A0, PE_12, PE_10);
+    Sistema sistema(tiempo, 0.3f, 22.0f, PA_0, D6, A0, PE_12, PE_10);
     // bucle infinito
     while (true) {}
 }
